64eea8a95a0d8153aff7ba22c26d0c2b926491c9
SomeRandomDude404 <163067675+somerandomdude404@users.noreply.github.com>
2026-07-30 21:37:27 -0700

    Create AvatarBone.cs

---
+++ DashX360.Avatar.Renderer/AvatarBone.cs
@@ -0,0 +1,12 @@
+namespace DashX360.Avatar.Renderer
+{
+    // Mirrors the XNA AvatarBone enumeration for skeleton hierarchy
+    public enum AvatarBone
+    {
+        Root, Spine, Spine1, Spine2, Neck, Head,
+        LeftShoulder, LeftElbow, LeftWrist, LeftHand,
+        RightShoulder, RightElbow, RightWrist, RightHand,
+        LeftHip, LeftKnee, LeftAnkle, LeftFoot,
+        RightHip, RightKnee, RightAnkle, RightFoot
+    }
+}
